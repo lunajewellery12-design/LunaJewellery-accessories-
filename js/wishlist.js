4c90d6cf76f37bf0241141e3fb068e209d37009d
lunajewellery12-design <lunajewellery12@gmail.com>
2026-07-20 17:41:44 +0300

    Add Wishlist functionality with CRUD operations
    Implement a Wishlist object for managing items.

---
+++ js/wishlist.js
@@ -0,0 +1,94 @@
+"use strict";
+
+/* ==========================================
+   LUNOVIA Jewellery
+   Wishlist
+========================================== */
+
+const Wishlist = {
+
+    items: [],
+
+    init() {
+
+        this.items = StorageManager.load(
+            StorageManager.keys.wishlist,
+            []
+        );
+
+        console.log("Wishlist Ready");
+
+    },
+
+    save() {
+
+        StorageManager.save(
+            StorageManager.keys.wishlist,
+            this.items
+        );
+
+    },
+
+    getItems() {
+
+        return this.items;
+
+    },
+
+    getCount() {
+
+        return this.items.length;
+
+    },
+
+    has(productId) {
+
+        return this.items.includes(productId);
+
+    },
+
+    add(productId) {
+
+        if (!this.has(productId)) {
+
+            this.items.push(productId);
+
+            this.save();
+
+        }
+
+    },
+
+    remove(productId) {
+
+        this.items = this.items.filter(
+            id => id !== productId
+        );
+
+        this.save();
+
+    },
+
+    toggle(productId) {
+
+        if (this.has(productId)) {
+
+            this.remove(productId);
+
+        } else {
+
+            this.add(productId);
+
+        }
+
+    },
+
+    clear() {
+
+        this.items = [];
+
+        this.save();
+
+    }
+
+};
